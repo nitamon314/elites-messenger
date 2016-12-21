e0ed0292bfd1b3d3c67dc7925fca4bd537d93302
nitamon314 <nitamon314@gmail.com>
2016-12-21 14:30:57 +0000

    実習10

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,6 +1,7 @@
 class TimelinesController < ApplicationController
   def index
-    @input_message = Timeline.new
+    #メッセージ入力
+    @input_message = params[:id] ? Timeline.find(params[:id]) : Timeline.new
     @timeline = Timeline.includes(:user).order('updated_at DESC')
   end
   
@@ -11,7 +12,18 @@ class TimelinesController < ApplicationController
     if timeline.valid?
       timeline.save!
     else
-      flash[:alert] = timeline.errors.full_message
+      flash[:alert] = timeline.errors.full_messages
+    end
+    redirect_to action: :index
+  end
+  
+  def update
+    timeline = Timeline.find(params[:id])
+    timeline.attributes = input_message_param
+    if timeline.valid?
+      timeline.save!
+    else
+      flash[:alert] = timeline.errors.full_messages
     end
     redirect_to action: :index
   end
